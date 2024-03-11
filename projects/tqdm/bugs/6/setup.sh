27cdf99755d497c04b1304f4c8459401643b8b4b
Dylan Callaghan <21831599@sun.ac.za>
2024-03-11 10:04:42 +0200

    Fixed pytest bug in tqdm.
    [Bugs]
    In the tqdm project, the tests are stored in the directory tqdm/tests.
    This presents a problem as pytest does not discover tests in this
    directory. To remedy this, the setup files for the tqdm versions were
    modified to create a pytest configuration file.
    
    [File's changed]
    	modified:   projects/tqdm/bugs/1/setup.sh
    	modified:   projects/tqdm/bugs/2/setup.sh
    	modified:   projects/tqdm/bugs/3/setup.sh
    	modified:   projects/tqdm/bugs/4/setup.sh
    	modified:   projects/tqdm/bugs/5/setup.sh
    	modified:   projects/tqdm/bugs/6/setup.sh
    	modified:   projects/tqdm/bugs/7/setup.sh
    	modified:   projects/tqdm/bugs/8/setup.sh
    	modified:   projects/tqdm/bugs/9/setup.sh

--- projects/tqdm/bugs/6/setup.sh
+++ projects/tqdm/bugs/6/setup.sh
@@ -1,5 +1,6 @@
 python setup.py install
-pip3 install nose 
+eval echo -e "[pytest]\\ntestpaths= tqdm/tests\\npython_files = tests_*.py" > pytest.ini
+pip3 install nose
 pip3 install flake8
 pip3 install coverage
 pip3 install pytest
