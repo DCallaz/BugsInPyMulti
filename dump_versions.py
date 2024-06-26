c8aeaa11fe61b0a02bb9cfa38364c5b52f4b3df9
Dylan Callaghan <21831599@sun.ac.za>
2024-06-26 16:28:20 +0200

    Added arguments to the dump_versions script.
    [Details]
    Added optional arguments to the dump_versions script for the test
    transplantation versions dir and fault location backtrack dir to use.
    
    [File's changed]
    	modified:   dump_versions.py

--- dump_versions.py
+++ dump_versions.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import os
+import getopt
 from os import path as osp
 
 def powerset(s):
@@ -11,28 +12,27 @@ def powerset(s):
         yield [ss for mask, ss in zip(masks, s) if i & mask]
 
 
-def get_versions(project, print_all=False):
+def get_versions(project, version_dir, backtrack_dir, print_all=False):
     vs = []
-    if (not osp.isdir(osp.join("versions", project))):
+    if (not osp.isdir(osp.join(version_dir, project))):
         print("ERROR: project", project, "not found")
         quit()
     sys.path.append(osp.join(os.getcwd(), "framework", "bin"))
     from backtrack import get_backtrack as backtrack
 
-    location_versions = open(osp.join("backtracks", project+"_backtrack.json"))
-    loc_js = json.loads(location_versions.read())
-
-    versions = [int(f.name) for f in os.scandir(osp.join("versions", project)) if f.is_dir()]
+    versions = [int(f.name) for f in os.scandir(osp.join(version_dir, project)) if f.is_dir()]
     versions.sort()
     for version in versions:
-        bugFile = open(osp.join("versions", project, str(version), "bugs.txt"))
+        bugFile = open(osp.join(version_dir, project, str(version), "bugs.txt"))
         faults = [int(line.split(",")[0]) for line in bugFile.readlines()]
-        faults.append(version)
+        # faults.append(version)
+        if (version not in faults):
+            continue
         faults = list(set(faults))
         faults.sort()
         s = (project, [])
         for fault in faults:
-            if (print_all or backtrack("backtracks", project, str(fault),
+            if (print_all or backtrack(backtrack_dir, project, str(fault),
                 str(version)) is not None):
                 s[1].append(fault)
         vs.append(s)
@@ -40,13 +40,35 @@ def get_versions(project, print_all=False):
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 2):
-        print("USAGE: python dump_versions.py <project> [all]")
+    # Process arguments
+    usage = "USAGE: python dump_versions.py [-h] [-v <version dir>] " \
+                "[-b <backtrack dir>] <project> [all]"
+    args = sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(args, "hv:b:", ["help"])
+    except getopt.GetoptError as err:
+        print(err)
+        print(usage)
         quit()
-    project = sys.argv[1]
+    opt_dict = dict(opts)
+    if ('-h' in opt_dict or '--help' in opt_dict):
+        print(usage)
+        quit()
+    version_dir = 'versions'
+    backtrack_dir = 'backtracks'
+    if ('-v' in opt_dict):
+        version_dir = opt_dict['-v']
+    if ('-b' in opt_dict):
+        backtrack_dir = opt_dict['-b']
+    if (len(args) < 1):
+        print("ERROR: please specify a project")
+        print(usage)
+        quit()
+    project = args[0]
     print_all = False
-    if (len(sys.argv) > 2 and sys.argv[2] == "all"):
+    if (len(args) > 1 and args[1] == "all"):
         print_all = True
-    versions = get_versions(project, print_all)
+    # Get versions
+    versions = get_versions(project, version_dir, backtrack_dir, print_all)
     for v in versions:
         print(v[0], *v[1], sep="-")
